907f2675a0942a4e3cc8b01910989675371a6f09
root <root@stg.stlawu.edu>
2018-03-28 12:12:55 -0400

    updated core and modules

--- sites/default/modules/slu/digital_collections.js
+++ sites/default/modules/slu/digital_collections.js
@@ -1,6 +1,7 @@
 Drupal.behaviors.digitalCollectionsBehavoir = function (context) {
   // term location in URL is hardcoded as 5th element
-  term = window.document.URL.split('/')[4];
+  arr = window.document.URL.split('/');
+  term = arr[2 + Drupal.settings.basePath.split('/').length];
   // Match against cck field class, 3rd parameter to new_fake_link is the URL relative to base_path()
   $('.field-field-dc-subject .field-item').each(function(i, dom) { new_fake_link(i, dom, 'collections/'+term+'/bysubject')});
   $('.field-field-dc-creator .field-item').each(function(i, dom) { new_fake_link(i, dom, 'collections/'+term+'/bycreator')});
@@ -12,6 +13,6 @@ Drupal.behaviors.digitalCollectionsBehavoir = function (context) {
 function new_fake_link(i, dom, url) {
   label = jQuery.trim($('div', dom).text());
   value = jQuery.trim(jQuery.trim($(dom).text()).slice(label.length));
-  label = $('div', dom).clone()
-  $(dom).empty().append(label).append('<a href="/' + url + '/' + value + '">' + value +'</a>');
+  label = $('div', dom).clone();
+  $(dom).empty().append(label).append('<a href="'+ Drupal.settings.basePath + url + '/' + value + '">' + value +'</a>');
 }
